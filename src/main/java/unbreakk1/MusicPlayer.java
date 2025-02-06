0552300343324b9c38e261fea8dc4c5765488eda
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-06 10:38:10 +0100

    added MusicPlayer & VideoPlayer Classes

---
+++ src/main/java/unbreakk1/MusicPlayer.java
@@ -0,0 +1,10 @@
+﻿package unbreakk1;
+
+public class MusicPlayer implements Playable
+{
+    @Override
+    public void play()
+    {
+        System.out.printf("Music playing...");
+    }
+}
---
+++ src/main/java/unbreakk1/VideoPlayer.java
@@ -0,0 +1,10 @@
+﻿package unbreakk1;
+
+public class VideoPlayer implements Playable
+{
+    @Override
+    public void play()
+    {
+        System.out.println("Video playing...");
+    }
+}
